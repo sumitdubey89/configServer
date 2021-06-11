3b0f632a3fea2febc420cfdcd05138612dc0dcd6
egrc3uh <sumit.dubey@mckesson.com>
2021-06-11 17:48:14 +0530

    initial

---
+++ src/test/java/epam/demo/configServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package epam.demo.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
